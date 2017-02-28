f991b7a4f4e665a0003f0cbed7b7bc4d5596bdcc
阿志 <129903445@qq.com>
2017-02-28 14:45:45 +0800

    历史登录的头像

--- js/common/main.js
+++ js/common/main.js
@@ -31,21 +31,54 @@ require(['jquery', 'bootstrap','common'],function(){
  * */
 (function(window){
 
+    //获取路径
     var pathname = window.location.pathname;
 
-    switch(pathname) {
-        case '/html/user/list.html':
-            console.log(234);
-            require(['userList']);
-            break;
-        case '/html/user/profile.html':
-            require(['userProfile']);
-            break;
-        case '/html/home/login':
-            require(['login']);
-            break;
+    /*
+    * 判断登录的状态
+    *
+    * 1.当前在登录页的时候，
+    * 没有session,则无需管
+    * 有session，跳转到首页
+    *
+    * 2.其他页
+    * 2.1没有session。则跳转到登录页
+    * 2.2有session，则无需管
+    * */
 
-    }
+   require(['jquery','jqueryCookie'],function($,undefined){
+
+        var sessionID= $.cookie('PHPSESSID');
+
+       //在登录之前判断状态验证
+       if(pathname==='/html/home/login.html'&&sessionID){
+           location.href='/';
+       }else if(pathname!=='/html/home/login.html'&&!sessionID){
+           location.href='/html/home/login.html';
+       }
+
+        //当没有发生页面跳转的时候，加载对应的js模块
+       switch(pathname) {
+           case '/html/user/list.html':
+               console.log(234);
+               require(['userList']);
+               break;
+           case '/html/user/profile.html':
+               require(['userProfile']);
+               break;
+           case '/html/home/login.html':
+               require(['login']);
+               break;
+
+       }
+
+
+
+   })
+
+
+
+
 
 
 }(window));
--- js/home/login.js
+++ js/home/login.js
@@ -1,7 +1,66 @@
 /**
  * Created by hom on 2017/2/25.
  */
-define(['jquery'],function($){
+define(['jquery','jqueryCookie'],function($,undefined){
 
-    console.log('login登录');
+    /*
+    * 展示用户的历史登录头像：
+    * 1.获取userInfo这个cookie的值
+    * 2.把cookie的字符串转换为对象
+    * 3.设置登录页的img-src为对象中的tc_avatar属性值如果没有，
+    * 就给一个默认的头像地址
+    *
+    *
+    *
+    * */
+    var userInfo=null;
+    try{
+        userInfo=JSON.parse($.cookie('userInfo'));
+    }catch(e){
+        userInfo={};
+    }
+    $('.login .acatar img').attr('src',userInfo.tc_avatar?userInfo.tc_avatar:'/uploads/default.png');
+
+
+
+
+    /*
+    * 登录页面实现的几个步骤：
+    * 1.先监听form表单的提交事件
+    * 2.事件回调return false阻止默认的事件
+    * 3.事件回调通过ajax的方式发送表单数据
+    * 4.如果返回的结果code为200的话，证明登录成功，跳转到首页
+    *
+    *
+    * */
+    //先监听form表单的提交事件
+    $('#form-login').on('submit',function(){
+
+        //事件回调通过ajax的方式发送表单数据
+        $.ajax({
+            url:'/v6/login',
+            type:'post',
+            data:$(this).serialize(),
+            //成功请求回调
+            success:function(data){
+                //如果返回的结果code为200的话，证明登录成功
+                if(data.code===200){
+
+                    $.cookie('userInfo',JSON.stringify(data.result),{path:'/'});
+                    //跳转到首页
+                    location.href='/';
+                }
+
+            }
+
+
+        });
+
+
+        //阻止事件的默认行为
+        return false;
+    });
+
+
+
 });
